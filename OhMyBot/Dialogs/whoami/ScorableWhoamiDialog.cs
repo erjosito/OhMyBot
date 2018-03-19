6bf49f5d3584efc056eb6120166ef459e347a476
erjosito <erjosito@hotmail.com>
2018-03-19 12:57:36 +0100

    username

--- OhMyBot/Dialogs/whoami/ScorableWhoamiDialog.cs
+++ OhMyBot/Dialogs/whoami/ScorableWhoamiDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
@@ -17,8 +18,15 @@ namespace OhMyBot.Dialogs.whoami
         public async Task StartAsync(IDialogContext context)
         {
             await context.PostAsync($"You have been shorcutted to the whoami dialog.{Environment.NewLine}You must be someone important");
-            await context.PostAsync($"Your must be " + context.UserData.GetValue<string>("Name"));
-
+            string Username;
+            if (context.UserData.TryGetValue("Name", out Username))
+            {
+                await context.PostAsync($"You must be " + Username);
+            }
+            else
+            {
+                await context.PostAsync($"I dont know who you are...");
+            }
             // State transition - complete this Dialog and remove it from the stack
             context.Done<object>(new object());
         }
