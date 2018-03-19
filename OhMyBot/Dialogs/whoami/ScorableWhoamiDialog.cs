df993693d994ab78feeb481c1f6f2aec311b2d5d
erjosito <erjosito@hotmail.com>
2018-03-19 13:18:58 +0100

    context.activity.from.id

--- OhMyBot/Dialogs/whoami/ScorableWhoamiDialog.cs
+++ OhMyBot/Dialogs/whoami/ScorableWhoamiDialog.cs
@@ -18,16 +18,14 @@ namespace OhMyBot.Dialogs.whoami
         // Entry point to the Dialog
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync($"You have been shorcutted to the whoami dialog.{Environment.NewLine}You must be someone important");
-            if (context.UserData.TryGetValue("Name", out Username))
-            {
-                await context.PostAsync($"You must be " + Username);
-            }
-            else
-            {
-                await context.PostAsync($"Context.Activity.From.Id: " + context.Activity.From.Id.ToString());
-                await context.PostAsync($"Context.Activity.ChannelId: " + context.Activity.ChannelId.ToString());
-            }
+            await context.PostAsync($"You have been shorcutted to the whoami dialog");
+            await context.PostAsync($"Context.Activity.From.Id: " + context.Activity.From.Id.ToString());
+            await context.PostAsync($"Context.Activity.From.Name: " + context.Activity.From.Name.ToString());
+            await context.PostAsync($"Context.Activity.Recipient.Id: " + context.Activity.Recipient.Id.ToString());
+            await context.PostAsync($"Context.Activity.Recipient.Name: " + context.Activity.Recipient.Name.ToString());
+            await context.PostAsync($"Context.Activity.ChannelId: " + context.Activity.ChannelId.ToString());
+            await context.PostAsync($"Context.Activity.ServiceUrl: " + context.Activity.ServiceUrl.ToString());
+            await context.PostAsync($"Context.Activity.LocalTimestamp: " + context.Activity.LocalTimestamp.ToString());
             // State transition - complete this Dialog and remove it from the stack
             context.Done<object>(new object());
         }
